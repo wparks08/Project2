eb154f0d798b9e0e537b12e160505cc015a28c54
Will Parks <wparks08@gmail.com>
2020-01-17 00:16:06 -0800

    add documentation, add middleware to validate password input

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,18 @@
-const router = require("express").Router();
 const db = require("../models");
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
 const bcrypt = require("bcryptjs");
 
 module.exports = function(app) {
+    //Provides the login page
     app.get("/user/login", (req, res) => {
-        res.render("login", { message: req.flash("error") });
+        res.render("login", {
+            message: req.flash("error")
+        });
     });
 
+    //This route accepts the POST data from a form containing two inputs: username and password.
+    //If the login is a success, user will be redirected to the route specified in successRedirect.
+    //Authentication failures are directed back to the login page, with an error saying what went wrong.
     app.post(
         "/user/login",
         passport.authenticate("local", {
@@ -18,11 +22,19 @@ module.exports = function(app) {
         })
     );
 
+    //Provides the registration page
     app.get("/user/register", (req, res) => {
-        res.render("register", { message: req.flash("error") });
+        res.render("register", {
+            message: req.flash("error")
+        });
     });
 
-    app.post("/user/create", (req, res) => {
+    //accepts POST data from the registration page. Passwords are provided as raw, plain text, and encrypted
+    //before they are persisted to the datastore. Passwords must also be at least 6 characters in length, enforced here.
+    //If the new user creation is successful, the user will be redirected to the login page. If not,
+    //they are directed back to the registration page with an error
+    //saying what went wrong.
+    app.post("/user/create", validatePasswordInput, (req, res) => {
         bcrypt.genSalt(10).then(salt => {
             bcrypt.hash(req.body.password, salt).then(hashedPassword => {
                 db.user
@@ -45,3 +57,17 @@ module.exports = function(app) {
         });
     });
 };
+
+function validatePasswordInput(req, res, next) {
+    if (req.body.password.length < 5) {
+        req.flash("error", "Password must be 6 or more characters.");
+        res.redirect("/user/register");
+        return;
+    } else if (req.body.password !== req.body.confirmPassword) {
+        req.flash("error", "Passwords didn't match!");
+        res.redirect("/user/register");
+        return;
+    } else {
+        return next();
+    }
+}
