8561ca12f7b901a2bbe2b7a2e2d6d006dcc4a88c
Will Parks <wparks08@gmail.com>
2020-01-17 18:48:06 -0800

    add event and venue models

---
+++ models/event.js
@@ -0,0 +1,20 @@
+module.exports = function(sequelize, DataTypes) {
+    const event = sequelize.define("event", {
+        name: DataTypes.STRING,
+        dateTimeStart: DataTypes.DATE,
+        dateTimeEnd: DataTypes.DATE
+    });
+
+    event.associate = function(models) {
+        //models.event.hasMany(models.user, {
+        //    as: "attendees",
+        //    through: "users_events"
+        //})
+
+        //models.event.belongsTo(models.venue, {
+        //    through: "venue_events"
+        //})
+    }
+
+    return event;
+}
