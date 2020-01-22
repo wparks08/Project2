7c3459b9c2500346b175d38526733090a0b0b361
Will Parks <wparks08@gmail.com>
2020-01-21 21:31:34 -0800

    refactor to render map.handlebars with lon and lat

--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -1,18 +1,21 @@
-var path = require("path");
 var IPGeolocationAPI = require("ip-geolocation-api-javascript-sdk");
-var GeolocationParams = require("ip-geolocation-api-javascript-sdk/GeolocationParams.js");
 
 module.exports = function(app) {
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./example.html"));
-    var ipgeolocationApi = new IPGeolocationAPI(
-      "b10f4262d4fa4a8cb7f4dc5bda0533e7",
-      false
-    );
-    function handleResponse(json) {
-      console.log(json);
-    }
-    ipgeolocationApi.getGeolocation(handleResponse);
-    var geolocationParams = new GeolocationParams();
-  });
+    app.get("/map", function(req, res) {
+        var ipgeolocationApi = new IPGeolocationAPI(
+            "b10f4262d4fa4a8cb7f4dc5bda0533e7",
+            false
+        );
+        function handleResponse(json) {
+            console.log(json);
+            console.log(json.latitude, json.longitude);
+
+            res.render("map", {
+                longitude: json.longitude,
+                latitude: json.latitude
+            });
+        }
+
+        ipgeolocationApi.getGeolocation(handleResponse);
+    });
 };
