9fa9c75e62aed6bf689e89eb5f944d13edb7668a
Will Parks <wparks08@gmail.com>
2020-01-21 21:30:51 -0800

    remove example.html route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,21 +4,6 @@ var IPGeolocationAPI = require("ip-geolocation-api-javascript-sdk");
 var GeolocationParams = require("ip-geolocation-api-javascript-sdk/GeolocationParams.js");
 
 module.exports = function (app) {
-  //added .get call to return the frontend html, however there is an issue with the 
-  //external js file as it calls the wrong html instead of a js file
-
-  app.get("/example.html", function (req, res) {
-    res.sendFile(path.join(__dirname, "../views/example.html"));
-    var ipgeolocationApi = new IPGeolocationAPI(
-      "b10f4262d4fa4a8cb7f4dc5bda0533e7",
-      false
-    );
-    function handleResponse(json) {
-      console.log(json.longitude);
-    }
-    ipgeolocationApi.getGeolocation(handleResponse);
-    var geolocationParams = new GeolocationParams();
-  });
   // Load index page
   app.get("/", function (req, res) {
     db.Example.findAll({}).then(function (dbExamples) {
