9799d1615d863b4b8071162c88b517f6d0db8175
Will Parks <wparks08@gmail.com>
2020-01-22 02:03:00 -0800

    api routes to save events and view saved events

--- routes/api/user.js
+++ routes/api/user.js
@@ -2,7 +2,45 @@ var router = require("express").Router();
 var db = require("../../models");
 
 router.post("/saveEvent/:eventId", (req, res) => {
-    //TODO add event to logged in user via association
+    if (req.user) {
+        db.user.findOne({
+            where: {
+                id: req.user.id
+            }
+        }).then(user => {
+            db.event.findOne({
+                where: {
+                    id: req.params.eventId
+                }
+            }).then(event => {
+                if (event) {
+                    user.addSaved(event);
+                    res.status(200).send("Event saved.");
+                } else {
+                    res.status(404).send("Event not found.")
+                }
+            })
+        })
+    } else {
+        res.status(401).send("Please login.");
+    }
 });
+
+router.get("/savedEvents", (req, res) => {
+    if (req.user) {
+        db.user.findOne({
+            where: {
+                id: req.user.id
+            },
+            include: {
+                model: db.event,
+                as: "saved"
+            }
+        }).then(user => {
+            console.log(user);
+            res.json(user.saved);
+        })
+    }
+})
 
 module.exports = router;
