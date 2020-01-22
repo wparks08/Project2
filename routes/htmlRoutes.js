9248587d915b23c3ad68dd02856bce6cb944d19d
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-01-21 18:44:43 -0800

    attempting to fix bug that returns wrong back end file. back end still tentative to work as it cannot return a 'window' object in the server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,27 @@
 var db = require("../models");
+var path = require("path");
+var IPGeolocationAPI = require("ip-geolocation-api-javascript-sdk");
+var GeolocationParams = require("ip-geolocation-api-javascript-sdk/GeolocationParams.js");
 
-module.exports = function(app) {
+module.exports = function (app) {
+  //added .get call to return the frontend html, however there is an issue with the 
+  //external js file as it calls the wrong html instead of a js file
+
+  app.get("/example.html", function (req, res) {
+    res.sendFile(path.join(__dirname, "../views/example.html"));
+    var ipgeolocationApi = new IPGeolocationAPI(
+      "b10f4262d4fa4a8cb7f4dc5bda0533e7",
+      false
+    );
+    function handleResponse(json) {
+      console.log(json.longitude);
+    }
+    ipgeolocationApi.getGeolocation(handleResponse);
+    var geolocationParams = new GeolocationParams();
+  });
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -12,8 +30,8 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,7 +39,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
--- routes/mapRoutes.js
+++ routes/mapRoutes.js
@@ -1,19 +1,18 @@
 var path = require("path");
-var IPGeolocationAPI = require('ip-geolocation-api-javascript-sdk');
-var GeolocationParams = require('ip-geolocation-api-javascript-sdk/GeolocationParams.js');
+var IPGeolocationAPI = require("ip-geolocation-api-javascript-sdk");
+var GeolocationParams = require("ip-geolocation-api-javascript-sdk/GeolocationParams.js");
 
-module.exports = function (app) {
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "./example.html"));
-
-        var ipgeolocationApi = new IPGeolocationAPI("b10f4262d4fa4a8cb7f4dc5bda0533e7", false);
-        function handleResponse(json) {
-            console.log(json);
-        }
-        ipgeolocationApi.getGeolocation(handleResponse);
-        var geolocationParams = new GeolocationParams();
-    });
-    app.post("*", function (req, res) {
-
-    })
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./example.html"));
+    var ipgeolocationApi = new IPGeolocationAPI(
+      "b10f4262d4fa4a8cb7f4dc5bda0533e7",
+      false
+    );
+    function handleResponse(json) {
+      console.log(json);
+    }
+    ipgeolocationApi.getGeolocation(handleResponse);
+    var geolocationParams = new GeolocationParams();
+  });
 };
