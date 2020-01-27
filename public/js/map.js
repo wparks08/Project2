f521375fd4d459e6085816d71ab693ab49351cd9
Will Parks <wparks08@gmail.com>
2020-01-27 09:58:15 -0800

    update to use browser native geolocation

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,18 @@
 function displayMap(longitude, latitude) {
-    var map = tt.map({
-        key: "i9SnrejXhmJUaG7iAQiT4yQAn6vtd8ML",
-        container: "map",
-        style: "tomtom://vector/1/basic-main",
-        center: new tt.LngLat(longitude, latitude),
-        zoom: 15
+    let Geolocation = window.navigator.geolocation;
+    Geolocation.getCurrentPosition(function(position) {
+        var map = tt.map({
+            key: "i9SnrejXhmJUaG7iAQiT4yQAn6vtd8ML",
+            container: "map",
+            style: "tomtom://vector/1/basic-main",
+            center: new tt.LngLat(position.coords.longitude, position.coords.latitude),
+            zoom: 15
+        });
+        map.addControl(new tt.FullscreenControl());
+        map.addControl(new tt.NavigationControl());
+
+        console.log(map);
+
+        return map;
     });
-    map.addControl(new tt.FullscreenControl());
-    map.addControl(new tt.NavigationControl());
-
-    console.log(map);
-
-    return map
 }
